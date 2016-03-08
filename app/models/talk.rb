b06668a27416ffae14a5c11cc815b44376efdb21
emilio2hd <emilio2hd@gmail.com>
2016-03-08 10:55:28 -0300

    Create validation to check if the talk's date is greater than today.

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -3,14 +3,10 @@ class Talk < ActiveRecord::Base
   validates :first_name, :last_name, length: { maximum: 10 }
   validates :title, uniqueness: { case_sensitive: false }
   validates :date, uniqueness: { scope: [:published, :time] }
-
-  def self.date_taken
-    I18n.t('.date.taken', value: I18n.l(date))
-  end
-
   validates :title, length: { maximum: 30 }
   validates :subtitle, length: { maximum: 45 }
   validates :target, length: { maximum: 65 }
+  validate :date_greater_than_today
 
   belongs_to :user
 
@@ -28,5 +24,13 @@ class Talk < ActiveRecord::Base
 
   def number_formated
     number ? '%03d' % number : ''
+  end
+
+  private
+
+  def date_greater_than_today
+    if date.is_a? Date
+      errors.add(:date, :greater_than_today, value: I18n.l(date, format: :default)) unless date > DateTime.now
+    end
   end
 end
